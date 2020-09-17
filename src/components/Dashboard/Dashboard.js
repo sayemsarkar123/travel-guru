81bad318aac2283b69408e65c4e929a72f0115cb
sayemsarkar123 <sayemsarkar123@gmail.com>
2020-09-17 19:22:40 +0600

    added react-google-map

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,8 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { LoginContext } from '../../App';
 import hotelsData from '../../fakeData/hotelsData';
+import locationData from '../../fakeData/locationData';
 import Header from '../Header/Header';
 import Hotel from '../Hotel/Hotel';
+import Map from '../Map/Map';
 import './Dashboard.css';
 
 const Dashboard = () => {
@@ -12,6 +14,10 @@ const Dashboard = () => {
   useEffect(() => {
     setHotels(hotelsData);
   }, []);
+  const [location, setLocation] = useState({});
+  useEffect(() => {
+    setLocation(locationData.find(place => place.address === loginData.place));
+  }, [loginData]);
   return (
       <Container>
         <Row>
@@ -27,7 +33,8 @@ const Dashboard = () => {
                 hotels.map(hotel => <Hotel key={hotel.id} hotel={hotel}></Hotel>)
               }
               </Col>
-              <Col md={6}>
+            <Col md={6}>
+              {loginData.place && <Map location={location} zoomLevel={17} ></Map>}
               </Col>
             </Row>
           </Col>
